4f441de98a4d0b358228a33157945d480f3e41e8
LOG <jm555jm@gmail.com>
2013-12-14 03:17:39 +0800

    [實驗]基本路徑處理
    - 尚未完成
    - 可讀取sd卡下的BAIObj資料夾的檔案並列在清單
    - 還不能選擇

--- App組/TestForApp2/src/com/example/testforapp2/ChooseObjActivity.java
+++ App組/TestForApp2/src/com/example/testforapp2/ChooseObjActivity.java
@@ -1,20 +1,26 @@
 package com.example.testforapp2;
 
-import android.os.Bundle;
+import java.io.File;
+
 import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class ChooseObjActivity extends Activity {
 	private Button buttonBack, buttonSelect;
 	private Singleton singleton;
-	
+	private TextView objListView;
+	private static final String TAG = "SDCardActivity";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -26,6 +32,7 @@ public class ChooseObjActivity extends Activity {
 		
 		buttonBack = (Button)this.findViewById(R.id.choose_obj_button_back);
 		buttonSelect = (Button)this.findViewById(R.id.choose_obj_button_select);
+		objListView = (TextView)this.findViewById(R.id.choose_obj_objlist);
 		
 		buttonBack.setOnClickListener(new Button.OnClickListener(){
 
@@ -91,5 +98,38 @@ public class ChooseObjActivity extends Activity {
 	protected void onStop() {
 		// TODO Auto-generated method stub
 		super.onStop();
+	}
+	
+	protected void onResume(){
+		super.onResume();
+		File sdCardDir = Environment.getExternalStorageDirectory();
+		File objDir = new File(sdCardDir, "BAIObj");
+        File[] list = objDir.listFiles();
+        StringBuilder sb = new StringBuilder();
+        this.listChildred(list, sb, "");
+        this.objListView.setText(sb.toString());
+	}
+	private void listChildred(File[] list, StringBuilder sb, String space) {
+		// TODO Auto-generated method stub
+		 if (list == null) {
+	            // 跳過空目錄
+	            Log.e(TAG, ">>>>>>>>>>>>>>>>");
+	            return;
+	        }
+	        for (File f : list) {
+	            String msg;
+	            if (f.isDirectory()) {
+	                msg = space + " + " + f.getName();
+	                sb.append(msg + "\n");
+	                Log.d(TAG, msg);
+	                // 往下一層
+	                this.listChildred(f.listFiles(), sb, space + "   ");
+	            }
+	            else {
+	                msg = space + " - " + f.getName();
+	                sb.append(msg + "\n");
+	                Log.d(TAG, msg);
+	            }
+	       }
 	}
 }
